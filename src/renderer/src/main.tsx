a854ac25116efefc5c6347e93225467e210a4707
Luca Davi <l.davi@reply.it>
2025-10-13 15:46:46 +0200

    fix: some style issue

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,9 +1,9 @@
+import "./global.css";
 import "./assets/main.css";
 
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import "./global.css";
 import { Toaster } from "./components/ui/sonner";
 
 createRoot(document.getElementById("root")!).render(
